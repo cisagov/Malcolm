a797b1c3fa48a050b7194186b763951789f18822
SG <13872653+mmguero@users.noreply.github.com>
2019-07-01 11:13:36 -0600

    set ownership of symlink for auth_setup from desktop

--- shared/bin/agg-init.sh
+++ shared/bin/agg-init.sh
@@ -26,6 +26,7 @@ if [[ -r "$SCRIPT_PATH"/common-init.sh ]]; then
       fi
       if [[ -r "$MAIN_USER_HOME"/Malcolm/scripts/auth_setup.sh ]]; then
         ln -r -s "$MAIN_USER_HOME"/Malcolm/scripts/auth_setup.sh "$MAIN_USER_HOME/Desktop/Malcolm Authentication Setup.sh"
+        chown -h "$MAIN_USER:$MAIN_USER" "$MAIN_USER_HOME/Desktop/Malcolm Authentication Setup.sh"
       fi
       rm -f "$MAIN_USER_HOME"/Malcolm/firstrun
     fi
