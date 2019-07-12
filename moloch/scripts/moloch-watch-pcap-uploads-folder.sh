05c64a7b99469302172fc951836b62042ed27959
SG <13872653+mmguero@users.noreply.github.com>
2019-07-12 13:19:17 -0600

    Decrease verbosity of log during PCAP upload; also decrease sleep throttling into pcap/upload directory

--- moloch/scripts/moloch-watch-pcap-uploads-folder.sh
+++ moloch/scripts/moloch-watch-pcap-uploads-folder.sh
@@ -16,15 +16,15 @@ do
   FILEMIME=$(file -b --mime-type "$NEWFILE")
   if [[ "$FILEMIME" == 'application/vnd.tcpdump.pcap' ]] || [[ "$FILEMIME" == 'application/x-pcapng' ]] || [[ "$FILEMAGIC" == *"pcap-ng"* ]]; then
     # a pcap file to be processed by dropping it into $PROCESS_DIR
-    sleep 0.25 && chown 1000:1000 "$NEWFILE" && (>&2 mv -v "$NEWFILE" "$PROCESS_DIR/")
+    sleep 0.1 && chown 1000:1000 "$NEWFILE" && (>&2 mv -v "$NEWFILE" "$PROCESS_DIR/")
 
   elif [[ -d "$ZEEK_UPLOAD_DIR" ]] && ( echo "$FILEMIME" | grep --quiet -P "(application/gzip|application/x-gzip|application/x-7z-compressed|application/x-bzip2|application/x-cpio|application/x-lzip|application/x-lzma|application/x-rar-compressed|application/x-tar|application/x-xz|application/zip)" ); then
     # looks like this is a compressed file, we're assuming it's a zeek log archive to be processed by filebeat
-    sleep 0.25 && chown 1000:1000 "$NEWFILE" && (>&2 mv -v "$NEWFILE" "$ZEEK_UPLOAD_DIR/")
+    sleep 0.1 && chown 1000:1000 "$NEWFILE" && (>&2 mv -v "$NEWFILE" "$ZEEK_UPLOAD_DIR/")
 
   else
     # unhandled file type uploaded, delete it
-    sleep 0.25 && (>&2 rm "$NEWFILE")
+    sleep 0.1 && (>&2 rm "$NEWFILE")
     echo "Removed \"$NEWFILE\", unhandled file type \"$FILEMIME\""
 
   fi
