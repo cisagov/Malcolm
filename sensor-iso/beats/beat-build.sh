90bd12ba2ab4575753e3c24a8559ccc8a7c8a8cf
SG <13872653+mmguero@users.noreply.github.com>
2020-01-22 14:40:58 -0700

    Revert "bump elastic to 7.5.2"
    This reverts commit 440c85980b4e58064e164294ee395cf503e93487.

--- sensor-iso/beats/beat-build.sh
+++ sensor-iso/beats/beat-build.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2018 Battelle Energy Alliance, LLC.  All rights reserved.
 
-VERSION="7.5.2"
+VERSION="7.5.1"
 THIRD_PARTY_BRANCH="master"
 while getopts b:v:t: opts; do
    case ${opts} in
