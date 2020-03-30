89330cb27111ea3315348b7b86a1e0e2854d2488
SG <13872653+mmguero@users.noreply.github.com>
2020-03-30 14:52:34 -0600

    clear out previous maps between restarts

--- logstash/scripts/logstash-start.sh
+++ logstash/scripts/logstash-start.sh
@@ -64,6 +64,7 @@ find "$PIPELINES_DIR" -mindepth 1 -maxdepth 1 -type d -print0 2>/dev/null | sort
 '
 
 # create filters for network segment and host mapping in the enrichment directory
+rm -f "$NETWORK_MAP_OUTPUT_FILTER"
 /usr/local/bin/ip-to-segment-logstash.py --mixed "$INPUT_MIXED_MAP" --segment "$INPUT_CIDR_MAP" --host "$INPUT_HOST_MAP" -o "$NETWORK_MAP_OUTPUT_FILTER"
 
 if [[ -z "$ES_EXTERNAL_HOSTS" ]]; then
