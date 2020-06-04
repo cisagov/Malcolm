33971a446d2332d7c30625c0d5bcae6b20926476
SG <13872653+mmguero@users.noreply.github.com>
2020-06-04 14:24:59 -0600

    added -w option to allow elasticsearch to be populated with logs before starting curator, elastalert

--- elastalert/elastalert-start.sh
+++ elastalert/elastalert-start.sh
@@ -6,23 +6,7 @@ set -e
 
 echo "Giving Elasticsearch at $ELASTICSEARCH_URL time to start..."
 
-elastic_search_status.sh
-
-sleep 5
-
-echo "Waiting until Elasticsearch has log data before starting..."
-
-# wait until at least one sessions2-* index exists
-until (( $(curl -fs -H'Content-Type: application/json' -XGET "$ELASTICSEARCH_URL/_cat/indices/sessions2-*" 2>/dev/null | wc -l) > 0 )) ; do
-  sleep 5
-done
-echo "Log indices exist."
-
-# wait until at least one record with @timestamp exists
-until curl -fs -H'Content-Type: application/json' -XPOST "$ELASTICSEARCH_URL/sessions2-*/_search" -d'{ "sort": { "@timestamp" : "desc" }, "size" : 1 }' >/dev/null 2>&1 ; do
-  sleep 5
-done
-echo "Logs exist."
+elastic_search_status.sh -w
 
 echo "Starting ElastAlert!"
 npm start
