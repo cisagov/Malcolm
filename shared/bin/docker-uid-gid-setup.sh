393c5a808d5d91ff6241038f9ab621689082204a
SG <13872653+mmguero@users.noreply.github.com>
2020-06-30 11:04:39 -0600

    work in progress on specifying uid/gid for docker process

--- shared/bin/docker-uid-gid-setup.sh
+++ shared/bin/docker-uid-gid-setup.sh
@@ -2,16 +2,25 @@
 
 set -e
 
-ENTRYPOINT_ARGS=("$@")
+unset ENTRYPOINT_CMD
+unset ENTRYPOINT_ARGS
+[ "$#" -ge 1 ] && ENTRYPOINT_CMD="$1" && [ "$#" -gt 1 ] && shift 1 && ENTRYPOINT_ARGS=( "$@" )
 USER_HOME="$(getent passwd ${PUSER} | cut -d: -f6)"
 
 usermod --non-unique --uid ${PUID:-${DEFAULT_UID}} ${PUSER} 2>&1
 groupmod --non-unique --gid ${PGID:-${DEFAULT_GID}} ${PGROUP} 2>&1
 
 su --shell $(readlink /proc/$$/exe) --preserve-environment ${PUSER} << EOF
+set -x
 export USER="${PUSER}"
 export HOME="${USER_HOME}"
 whoami
 id
-"${ENTRYPOINT_ARGS[@]}"
+if [ ! -z "${ENTRYPOINT_CMD}" ]; then
+  if [ -z "${ENTRYPOINT_ARGS}" ]; then
+    "${ENTRYPOINT_CMD}"
+  else
+    "${ENTRYPOINT_CMD}" $(printf "%q " "${ENTRYPOINT_ARGS[@]}")
+  fi
+fi
 EOF
