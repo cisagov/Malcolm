764e2e6609748324455203b94624a1cd9e8c4884
SG <13872653+mmguero@users.noreply.github.com>
2021-11-04 15:39:33 -0600

    testing workflow for github

--- sensor-iso/build.sh
+++ sensor-iso/build.sh
@@ -58,31 +58,32 @@ if [ -d "$WORKDIR" ]; then
   sed -i "s@\(/etc/capture_storage_format\)@\1.none@g" ./config/includes.binary/install/preseed_minimal.cfg
 
   # create a hook for installing Python packages required by interface
-  if [ -f "$SCRIPT_PATH/interface/requirements.txt" ]; then
-    echo "#!/bin/sh" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
-    echo "export LC_ALL=C.UTF-8" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
-    echo "export LANG=C.UTF-8" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
-    echo -n "pip3 install --system --no-compile --no-cache-dir --force-reinstall --upgrade" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
-    while read LINE; do
-      echo -n -e " \\\\\n  $LINE" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
-    done <"$SCRIPT_PATH/interface/requirements.txt"
-    echo "" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
-    chmod +x ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
-  fi
+  # TODO: commenting out to test build ISO in github action
+  # if [ -f "$SCRIPT_PATH/interface/requirements.txt" ]; then
+  #   echo "#!/bin/sh" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
+  #   echo "export LC_ALL=C.UTF-8" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
+  #   echo "export LANG=C.UTF-8" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
+  #   echo -n "pip3 install --system --no-compile --no-cache-dir --force-reinstall --upgrade" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
+  #   while read LINE; do
+  #     echo -n -e " \\\\\n  $LINE" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
+  #   done <"$SCRIPT_PATH/interface/requirements.txt"
+  #   echo "" >> ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
+  #   chmod +x ./config/hooks/normal/0168-pip-sensor-interface-installs.hook.chroot
+  # fi
 
-  # make sure we install the firmwares, etc.
-  for PKG in firmware-linux \
-             firmware-linux-free \
-             firmware-linux-nonfree \
-             firmware-misc-nonfree \
-             firmware-amd-graphics \
-             firmware-iwlwifi \
-             firmware-atheros \
-             linux-headers-amd64; do
-    echo "$PKG" >> ./config/package-lists/firmwares.list.chroot
-  done
+  # # make sure we install the firmwares, etc.
+  # for PKG in firmware-linux \
+  #            firmware-linux-free \
+  #            firmware-linux-nonfree \
+  #            firmware-misc-nonfree \
+  #            firmware-amd-graphics \
+  #            firmware-iwlwifi \
+  #            firmware-atheros \
+  #            linux-headers-amd64; do
+  #   echo "$PKG" >> ./config/package-lists/firmwares.list.chroot
+  # done
 
-  mkdir -p ./config/includes.chroot/opt/hedgehog_install_artifacts
+  # mkdir -p ./config/includes.chroot/opt/hedgehog_install_artifacts
 
   # copy the interface code into place for the resultant image
   mkdir -p ./config/includes.chroot/opt
@@ -108,35 +109,36 @@ if [ -d "$WORKDIR" ]; then
   echo "BUG_REPORT_URL=\"https://github.com/idaholab/malcolm/issues\""  >> ./config/includes.chroot/opt/sensor/.os-info
 
   # grab maxmind geoip database files, iana ipv4 address ranges, wireshark oui lists, etc.
-  mkdir -p "$SCRIPT_PATH/arkime/etc"
-  pushd "$SCRIPT_PATH/arkime/etc"
-  MAXMIND_GEOIP_DB_LICENSE_KEY=""
-  if [[ -f "$SCRIPT_PATH/shared/maxmind_license.txt" ]]; then
-    MAXMIND_GEOIP_DB_LICENSE_KEY="$(cat "$SCRIPT_PATH/shared/maxmind_license.txt" | head -n 1)"
-    if [[ ${#MAXMIND_GEOIP_DB_LICENSE_KEY} -gt 1 ]]; then
-      for DB in ASN Country City; do
-        curl -s -S -L -o "GeoLite2-$DB.mmdb.tar.gz" "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-$DB&license_key=$MAXMIND_GEOIP_DB_LICENSE_KEY&suffix=tar.gz"
-        tar xvf "GeoLite2-$DB.mmdb.tar.gz" --wildcards --no-anchored '*.mmdb' --strip=1 --no-same-owner
-        rm -f "GeoLite2-$DB.mmdb.tar.gz"
-      done
-    fi
-  fi
-  curl -s -S -L -o ipv4-address-space.csv "https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv"
-  curl -s -S -L -o oui.txt "https://raw.githubusercontent.com/wireshark/wireshark/master/manuf"
-  popd >/dev/null 2>&1
+  # TODO: commenting out to test build ISO in github action
+  # mkdir -p "$SCRIPT_PATH/arkime/etc"
+  # pushd "$SCRIPT_PATH/arkime/etc"
+  # MAXMIND_GEOIP_DB_LICENSE_KEY=""
+  # if [[ -f "$SCRIPT_PATH/shared/maxmind_license.txt" ]]; then
+  #   MAXMIND_GEOIP_DB_LICENSE_KEY="$(cat "$SCRIPT_PATH/shared/maxmind_license.txt" | head -n 1)"
+  #   if [[ ${#MAXMIND_GEOIP_DB_LICENSE_KEY} -gt 1 ]]; then
+  #     for DB in ASN Country City; do
+  #       curl -s -S -L -o "GeoLite2-$DB.mmdb.tar.gz" "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-$DB&license_key=$MAXMIND_GEOIP_DB_LICENSE_KEY&suffix=tar.gz"
+  #       tar xvf "GeoLite2-$DB.mmdb.tar.gz" --wildcards --no-anchored '*.mmdb' --strip=1 --no-same-owner
+  #       rm -f "GeoLite2-$DB.mmdb.tar.gz"
+  #     done
+  #   fi
+  # fi
+  # curl -s -S -L -o ipv4-address-space.csv "https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv"
+  # curl -s -S -L -o oui.txt "https://raw.githubusercontent.com/wireshark/wireshark/master/manuf"
+  # popd >/dev/null 2>&1
 
-  # clone and build Arkime .deb package in its own clean environment (rather than in hooks/)
-  bash "$SCRIPT_PATH/arkime/build-docker-image.sh"
-  docker run --rm -v "$SCRIPT_PATH"/arkime:/build arkime-build:latest -o /build
-  cp "$SCRIPT_PATH/arkime"/*.deb ./config/includes.chroot/opt/hedgehog_install_artifacts/
-  mv "$SCRIPT_PATH/arkime"/*.deb ./config/packages.chroot/
+  # # clone and build Arkime .deb package in its own clean environment (rather than in hooks/)
+  # bash "$SCRIPT_PATH/arkime/build-docker-image.sh"
+  # docker run --rm -v "$SCRIPT_PATH"/arkime:/build arkime-build:latest -o /build
+  # cp "$SCRIPT_PATH/arkime"/*.deb ./config/includes.chroot/opt/hedgehog_install_artifacts/
+  # mv "$SCRIPT_PATH/arkime"/*.deb ./config/packages.chroot/
 
-  # clone and build custom protologbeat from github for logging temperature, etc.
-  mkdir -p ./config/includes.chroot/usr/local/bin/
-  bash "$SCRIPT_PATH/beats/build-docker-image.sh"
-  bash "$SCRIPT_PATH/beats/beat-build.sh" -b "https://github.com/mmguero-dev/protologbeat" -t "es_7_10_2_compat"
-  cp github.com_mmguero-dev_protologbeat/protologbeat ./config/includes.chroot/opt/hedgehog_install_artifacts/
-  mv github.com_mmguero-dev_protologbeat/protologbeat ./config/includes.chroot/usr/local/bin
+  # # clone and build custom protologbeat from github for logging temperature, etc.
+  # mkdir -p ./config/includes.chroot/usr/local/bin/
+  # bash "$SCRIPT_PATH/beats/build-docker-image.sh"
+  # bash "$SCRIPT_PATH/beats/beat-build.sh" -b "https://github.com/mmguero-dev/protologbeat" -t "es_7_10_2_compat"
+  # cp github.com_mmguero-dev_protologbeat/protologbeat ./config/includes.chroot/opt/hedgehog_install_artifacts/
+  # mv github.com_mmguero-dev_protologbeat/protologbeat ./config/includes.chroot/usr/local/bin
 
   # format and copy documentation
   pushd "$SCRIPT_PATH/"
