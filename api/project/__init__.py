36695c28a53d9fbacc57c36a08d62e21e1732a3b
SG <13872653+mmguero@users.noreply.github.com>
2021-12-22 07:36:42 -0700

    documentation for API

--- api/project/__init__.py
+++ api/project/__init__.py
@@ -21,6 +21,23 @@ opensearch_dsl.connections.create_connection(hosts=[app.config["OPENSEARCH_URL"]
 
 
 def gettimes(args):
+    """Parses 'from' and 'to' times out of the provided dictionary, returning
+    two datetime objects
+
+    Parameters
+    ----------
+    args : dict
+        The dictionary which should contain 'from' and 'to' times. Missing
+        times are returned as None. e.g., gettimes(request.args). Times
+        can be UNIX time integers represented as strings or strings
+        of various formats, in which case a "best guess" conversion is done.
+        If no time zone information is provided, UTC is assumed.
+
+    Returns
+    -------
+    return start_time, end_time
+        datetime objects representing the start and end time for a query
+    """
     if start_time_str := args.get("from"):
         start_time = (
             datetime.utcfromtimestamp(int(start_time_str))
@@ -42,6 +59,20 @@ def gettimes(args):
 @app.route("/protocols")
 @app.route("/services")
 def protocols():
+    """Returns the protocols found in network data for a specified period
+
+    Parameters
+    ----------
+    from : string
+        The beginning of the time period (see gettimes for format)
+    to : string
+        The ending of the time period (see gettimes for format)
+
+    Returns
+    -------
+    return json
+        jsonified OpenSearch result set containing protocols
+    """
     start_time, end_time = gettimes(request.args)
     return jsonify(
         start_time=start_time.strftime("%Y/%m/%d %H:%M:%S") if start_time else "",
@@ -51,12 +82,40 @@ def protocols():
 
 @app.route("/indices")
 def indices():
+    """Provide a list of indices in the OpenSearch data store
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    indices
+        The output of _cat/indices?format=json from the OpenSearch API
+    """
     return jsonify(indices=requests.get(f'{app.config["OPENSEARCH_URL"]}/_cat/indices?format=json').json())
 
 
 @app.route("/")
 @app.route("/version")
 def version():
+    """Provides version information about Malcolm and the underlying OpenSearch instance
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    version
+        a string containing the Malcolm version (e.g., "5.1.0")
+    built
+        a string containing the Malcolm build timestamp (e.g., "2021-12-22T14:13:26Z")
+    sha
+        a string containing the last commit sha from the Malcolm source repository (e.g., "11540a7")
+    opensearch
+        a JSON structure containing basic OpenSearch version information
+    opensearch_health
+        a JSON structure containing OpenSearch cluster health
+    """
     return jsonify(
         version=app.config["MALCOLM_VERSION"],
         built=app.config["BUILD_DATE"],
@@ -68,4 +127,14 @@ def version():
 
 @app.route("/ping")
 def ping():
+    """Says 'pong' (for a simple health check)
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    pong
+        a string containing 'pong'
+    """
     return jsonify(ping="pong")
