99254ff6b3a57346ee65b84c6dee66ddf54e7f9d
SG <13872653+mmguero@users.noreply.github.com>
2022-01-20 10:22:30 -0700

    Revert "temporarily point to my fork with @https://github.com/kkvarfordt/icsnpp-opcua-binary/tree/server-proto-ver-fix's fixes while testing the fix for cisagov/icsnpp-opcua-binary#2"
    This reverts commit e5dd454d0f57a3c81f7a628df7989264581bac45.

--- shared/bin/zeek_install_plugins.sh
+++ shared/bin/zeek_install_plugins.sh
@@ -77,7 +77,7 @@ ZKG_GITHUB_URLS=(
   "https://github.com/cisagov/icsnpp-enip"
   "https://github.com/cisagov/icsnpp-ethercat"
   "https://github.com/cisagov/icsnpp-modbus"
-  "https://github.com/mmguero-dev/icsnpp-opcua-binary|versions"
+  "https://github.com/cisagov/icsnpp-opcua-binary"
   "https://github.com/corelight/callstranger-detector"
   "https://github.com/corelight/CVE-2020-16898"
   "https://github.com/corelight/CVE-2021-31166"
