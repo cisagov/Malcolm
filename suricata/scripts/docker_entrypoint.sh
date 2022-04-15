9027310ef387c07ba8f2bd58dc2a8333ff81872d
Seth Grover <mero.mero.guero@gmail.com>
2022-04-15 14:42:43 -0600

    "document" suricata_config_populate.sh env. variables

--- suricata/scripts/docker_entrypoint.sh
+++ suricata/scripts/docker_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# modify suricata.yaml according to environment variables
 /usr/local/bin/suricata_config_populate.sh >&2
 
 # start supervisor (which will spawn pcap-suricata, cron, etc.) or whatever the default command is
