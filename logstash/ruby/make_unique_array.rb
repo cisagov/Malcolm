95baa55e17f096484cdc3f960702972b2a7aee62
Seth Grover <mero.mero.guero@gmail.com>
2022-04-26 13:38:03 -0600

    prune 'failed' as a network protocol from suricata events

--- logstash/ruby/make_unique_array.rb
+++ logstash/ruby/make_unique_array.rb
@@ -4,6 +4,13 @@ end
 
 def register(params)
   @field = params["field"]
+  _prune = params["prune"]
+  if !_prune.is_a?(Array) then
+    _newPrune = Array.new
+    _newPrune.push(_prune) unless _prune.nil?
+    _prune = _newPrune
+  end
+  @prune = _prune
 end
 
 def filter(event)
@@ -14,7 +21,7 @@ def filter(event)
       _newVals.push(_vals)
       _vals = _newVals
     end
-    event.set("#{@field}", _vals.uniq)
+    event.set("#{@field}", _vals.uniq.reject {|x| @prune.include? x})
   end
   [event]
 end
@@ -40,6 +47,30 @@ test "has no duplicates" do
   end
 
   in_event { { "sourcefield" => [ "a", "b", "c" ] } }
+
+  expect("result to be equal") do |events|
+    events.first.get("sourcefield").length == 3
+  end
+end
+
+test "array needs prune" do
+  parameters do
+    { "field" => "sourcefield", "prune" => "x" }
+  end
+
+  in_event { { "sourcefield" => [ "a", "b", "a", "x" ] } }
+
+  expect("result to be equal") do |events|
+    events.first.get("sourcefield").length == 2
+  end
+end
+
+test "array needs prune from array" do
+  parameters do
+    { "field" => "sourcefield", "prune" => [ "y", "z" ] }
+  end
+
+  in_event { { "sourcefield" => [ "a", "b", "a", "x", "y", "z" ] } }
 
   expect("result to be equal") do |events|
     events.first.get("sourcefield").length == 3
