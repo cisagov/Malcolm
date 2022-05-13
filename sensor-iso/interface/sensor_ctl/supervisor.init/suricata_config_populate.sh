95432dd018a6827658085da5588f0a8bdb6b0e6b
Seth Grover <mero.mero.guero@gmail.com>
2022-05-13 07:55:17 -0600

    fix custom rules not getting populated on hedgehog

--- sensor-iso/interface/sensor_ctl/supervisor.init/suricata_config_populate.sh
+++ sensor-iso/interface/sensor_ctl/supervisor.init/suricata_config_populate.sh
@@ -2,5 +2,7 @@
 
 if [[ -n $SUPERVISOR_PATH ]] && [[ -r /usr/local/bin/suricata_config_populate.py ]]; then
     # all arguments are controlled via environment variables sourced from control_vars.conf
+    SURICATA_CUSTOM_RULES_DIR="$SUPERVISOR_PATH"/suricata/rules
+    [[ -d "$SURICATA_CUSTOM_RULES_DIR" ]] && export SURICATA_CUSTOM_RULES_DIR
     python3 /usr/local/bin/suricata_config_populate.py
 fi
