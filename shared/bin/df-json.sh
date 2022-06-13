e9903ef8dce7ac7b9dd8980c4655c22134959ace
Seth Grover <mero.mero.guero@gmail.com>
2022-06-13 11:47:55 -0600

    sensor fluentbit work in progress

---
+++ shared/bin/df-json.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+df --exclude-type=tmpfs --exclude-type=devtmpfs --block-size=1 --local --no-sync --output=target,size,used,avail 2>/dev/null | \
+  jq -R -c -M -s '
+    {df: [
+      split("\n") |
+      .[] |
+      if test("^/") then
+        gsub(" +"; " ") | split(" ") | {target: .[0], size: .[1], used: .[2], avail: .[3]}
+      else
+        empty
+      end
+    ]}'
