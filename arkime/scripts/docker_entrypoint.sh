28fd9d87c78c67a4ff3d9ef7d19f41f8f7d9922e
SG <mero.mero.guero@gmail.com>
2022-08-16 10:14:19 -0600

    work in progress specifying the opensearch connection info

--- arkime/scripts/docker_entrypoint.sh
+++ arkime/scripts/docker_entrypoint.sh
@@ -5,7 +5,7 @@ OPENSEARCH_URL=${OPENSEARCH_URL:-"http://opensearch:9200"}
 
 if [[ -r "${ARKIME_DIR}"/etc/config.orig.ini ]]; then
     cp "${ARKIME_DIR}"/etc/config.orig.ini "${ARKIME_DIR}"/etc/config.ini
-    sed -i "s/^\(elasticsearch=\).*/\1"${OPENSEARCH_URL}"/" "${ARKIME_DIR}"/etc/config.ini
+    sed -i "s|^\(elasticsearch=\).*|\1"${OPENSEARCH_URL}"|" "${ARKIME_DIR}"/etc/config.ini
 fi
 
 # start supervisor or whatever the default command is
