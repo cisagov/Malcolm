5f75ce1fe1a144a434addb5716e371e9cab9f222
SG <mero.mero.guero@gmail.com>
2022-10-24 14:16:43 -0600

    initialize arkime certs before the rest of the initialzation

--- arkime/scripts/initarkime.sh
+++ arkime/scripts/initarkime.sh
@@ -19,6 +19,19 @@ OPENSEARCH_URL_FULL="$(grep -Pi '^elasticsearch\s*=' $ARKIME_DIR/etc/config.ini 
 
 rm -f /var/run/arkime/initialized /var/run/arkime/runwise
 
+# make sure TLS certificates exist prior to starting up
+CERT_FILE=$ARKIME_DIR/etc/viewer.crt
+KEY_FILE=$ARKIME_DIR/etc/viewer.key
+if ( [[ ! -f "$CERT_FILE" ]] || [[ ! -f "$KEY_FILE" ]] ) && [[ -x /usr/local/bin/self_signed_key_gen.sh ]]; then
+  rm -f "$CERT_FILE" "$KEY_FILE" ./newcerts
+  pushd $ARKIME_DIR/etc/ >/dev/null 2>&1
+  /usr/local/bin/self_signed_key_gen.sh -n -o ./newcerts >/dev/null 2>&1
+  mv ./newcerts/server.crt "$CERT_FILE"
+  mv ./newcerts/server.key "$KEY_FILE"
+  rm -rf ./newcerts
+  popd >/dev/null 2>&1
+fi
+
 echo "Giving OpenSearch time to start..."
 /opt/opensearch_status.sh 2>&1 && echo "OpenSearch is running!"
 
--- arkime/scripts/viewer_service.sh
+++ arkime/scripts/viewer_service.sh
@@ -2,19 +2,6 @@
 
 # Copyright (c) 2022 Battelle Energy Alliance, LLC.  All rights reserved.
 
-# make sure TLS certificates exist prior to starting up
-CERT_FILE=$ARKIME_DIR/etc/viewer.crt
-KEY_FILE=$ARKIME_DIR/etc/viewer.key
-if ( [[ ! -f "$CERT_FILE" ]] || [[ ! -f "$KEY_FILE" ]] ) && [[ -x /usr/local/bin/self_signed_key_gen.sh ]]; then
-  rm -f "$CERT_FILE" "$KEY_FILE" ./newcerts
-  pushd $ARKIME_DIR/etc/ >/dev/null 2>&1
-  /usr/local/bin/self_signed_key_gen.sh -n -o ./newcerts >/dev/null 2>&1
-  mv ./newcerts/server.crt "$CERT_FILE"
-  mv ./newcerts/server.key "$KEY_FILE"
-  rm -rf ./newcerts
-  popd >/dev/null 2>&1
-fi
-
 while true; do
   if [[ -f /var/run/arkime/initialized && "$VIEWER" == "on" ]]; then
     echo "Launch viewer..."
