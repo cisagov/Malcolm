32365e34017dec07d705eb289f6c0591f4dc3b81
Seth Grover <mero.mero.guero@gmail.com>
2023-01-10 13:23:06 -0700

    make script for backup/restore of netbox config data

--- scripts/demo/reset_and_auto_populate.sh
+++ scripts/demo/reset_and_auto_populate.sh
@@ -274,13 +274,7 @@ if [[ -f "$MALCOLM_DOCKER_COMPOSE" ]] && \
   if [[ -n "$NETBOX_BACKUP_FILE" ]] && [[ -f "$NETBOX_BACKUP_FILE" ]]; then
     # restore the netbox backup
     [[ -n $VERBOSE_FLAG ]] && echo "Restoring NetBox database backup" >&2
-    set +e
-    docker-compose -f "$MALCOLM_FILE" exec -u $(id -u) netbox-postgres dropdb -U netbox netbox --force
-    sleep 5
-    docker-compose -f "$MALCOLM_FILE" exec -u $(id -u) netbox-postgres createdb -U netbox netbox
-    gunzip < "$NETBOX_BACKUP_FILE" | docker-compose -f "$MALCOLM_FILE" exec -u $(id -u) -T netbox-postgres psql -U netbox
-    docker-compose -f "$MALCOLM_FILE" exec -u $(id -u) netbox /opt/netbox/netbox/manage.py migrate
-    set -e
+    ./scripts/netbox-restore $VERBOSE_FLAG -f "$MALCOLM_FILE" --netbox-restore "$NETBOX_BACKUP_FILE" || true
   fi
 
   if (( ${#PCAP_FILES_ADJUSTED[@]} > 0 )) || (( ${#PCAP_FILES_NOT_ADJUSTED[@]} > 0 )); then
