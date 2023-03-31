02a6d1e3392bd20ef387625ba69d93daaff0ae5e
Seth Grover <mero.mero.guero@gmail.com>
2023-03-31 16:00:10 -0600

    work in progress for some python refactoring

--- shared/bin/malcolm_utils.py
+++ shared/bin/malcolm_utils.py
@@ -15,6 +15,110 @@ from base64 import b64decode
 from tempfile import NamedTemporaryFile
 from Crypto.Cipher import AES
 
+
+###################################################################################################
+# urlencode each character of a string
+def aggressive_url_encode(string):
+    return "".join("%{0:0>2}".format(format(ord(char), "x")) for char in string)
+
+
+###################################################################################################
+# atomic integer class and context manager
+class AtomicInt:
+    def __init__(self, value=0):
+        self.val = RawValue('i', value)
+        self.lock = Lock()
+
+    def increment(self):
+        with self.lock:
+            self.val.value += 1
+            return self.val.value
+
+    def decrement(self):
+        with self.lock:
+            self.val.value -= 1
+            return self.val.value
+
+    def value(self):
+        with self.lock:
+            return self.val.value
+
+    def __enter__(self):
+        return self.increment()
+
+    def __exit__(self, type, value, traceback):
+        return self.decrement()
+
+
+###################################################################################################
+# if a string starts with 'base64:', decode it, otherwise return it as-is
+def base64_decode_if_prefixed(s: str):
+    if s.startswith('base64:'):
+        return b64decode(s[7:]).decode('utf-8')
+    else:
+        return s
+
+
+###################################################################################################
+# an OrderedDict that locks itself and unlocks itself as a context manager
+class ContextLockedOrderedDict(OrderedDict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lock = Lock()
+
+    def __enter__(self):
+        self.lock.acquire()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.lock.release()
+        return self
+
+
+###################################################################################################
+# convenience routine for deep-getting a value from a dictionary
+def deep_get(d, keys, default=None):
+    k = get_iterable(keys)
+    if d is None:
+        return default
+    if not keys:
+        return d
+    return deep_get(d.get(k[0]), k[1:], default)
+
+
+###################################################################################################
+# convenience routine for setting-getting a value into a dictionary
+def deep_set(d, keys, value, deleteIfNone=False):
+    k = get_iterable(keys)
+    for key in k[:-1]:
+        if (key not in d) or (not isinstance(d[key], dict)):
+            d[key] = dict()
+        d = d[key]
+    d[k[-1]] = value
+    if (deleteIfNone == True) and (value is None):
+        d.pop(k[-1], None)
+
+
+###################################################################################################
+# recursive dictionary key search
+def dictsearch(d, target):
+    val = filter(
+        None, [[b] if a == target else dictsearch(b, target) if isinstance(b, dict) else None for a, b in d.items()]
+    )
+    return [i for b in val for i in b]
+
+
+###################################################################################################
+# print to stderr
+def eprint(*args, **kwargs):
+    if "timestamp" in kwargs and kwargs["timestamp"]:
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), *args, file=sys.stderr, **kwargs)
+    else:
+        print(*args, file=sys.stderr, **kwargs)
+    if "flush" in kwargs and kwargs["flush"]:
+        sys.stderr.flush()
+
+
 ###################################################################################################
 # EVP_BytesToKey
 #
@@ -56,152 +160,13 @@ def EVP_BytesToKey(key_length: int, iv_length: int, md, salt: bytes, data: bytes
     return key, iv
 
 
-def base64_decode_if_prefixed(s: str):
-    if s.startswith('base64:'):
-        return b64decode(s[7:]).decode('utf-8')
-    else:
-        return s
-
-
-def LoadStrIfJson(jsonStr):
-    try:
-        return json.loads(jsonStr)
-    except ValueError as e:
-        return None
-
-
-def LoadFileIfJson(fileHandle):
-    try:
-        return json.load(fileHandle)
-    except ValueError as e:
-        return None
-
-
-@contextlib.contextmanager
-def temporary_filename(suffix=None):
-    try:
-        f = tempfile.NamedTemporaryFile(suffix=suffix, delete=False)
-        tmp_name = f.name
-        f.close()
-        yield tmp_name
-    finally:
-        os.unlink(tmp_name)
-
-
 ###################################################################################################
-@contextlib.contextmanager
-def pushd(directory):
-    prevDir = os.getcwd()
-    os.chdir(directory)
-    try:
-        yield
-    finally:
-        os.chdir(prevDir)
-
-
-###################################################################################################
+# return just about any object as an iterable
 def get_iterable(x):
     if isinstance(x, Iterable) and not isinstance(x, str):
         return x
     else:
         return (x,)
-
-
-###################################################################################################
-def deep_get(d, keys, default=None):
-    k = get_iterable(keys)
-    if d is None:
-        return default
-    if not keys:
-        return d
-    return deep_get(d.get(k[0]), k[1:], default)
-
-
-###################################################################################################
-def deep_set(d, keys, value, deleteIfNone=False):
-    k = get_iterable(keys)
-    for key in k[:-1]:
-        if (key not in d) or (not isinstance(d[key], dict)):
-            d[key] = dict()
-        d = d[key]
-    d[k[-1]] = value
-    if (deleteIfNone == True) and (value is None):
-        d.pop(k[-1], None)
-
-
-###################################################################################################
-# open a file and close it, updating its access time
-def touch(filename):
-    open(filename, 'a').close()
-    os.utime(filename, None)
-
-
-###################################################################################################
-# print to stderr
-def eprint(*args, **kwargs):
-    if "timestamp" in kwargs and kwargs["timestamp"]:
-        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), *args, file=sys.stderr, **kwargs)
-    else:
-        print(*args, file=sys.stderr, **kwargs)
-    if "flush" in kwargs and kwargs["flush"]:
-        sys.stderr.flush()
-
-
-###################################################################################################
-# urlencode each character of a string
-def aggressive_url_encode(string):
-    return "".join("%{0:0>2}".format(format(ord(char), "x")) for char in string)
-
-
-###################################################################################################
-# strip a prefix from the beginning of a string if needed
-def remove_prefix(text, prefix):
-    if (len(prefix) > 0) and text.startswith(prefix):
-        return text[len(prefix) :]
-    else:
-        return text
-
-
-###################################################################################################
-# nice human-readable file sizes
-def sizeof_fmt(num, suffix='B'):
-    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-        if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
-        num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
-
-
-###################################################################################################
-# convenient boolean argument parsing
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise ValueError('Boolean value expected.')
-
-
-###################################################################################################
-def val2bool(v):
-    try:
-        if v is None:
-            return False
-        elif isinstance(v, bool):
-            return v
-        elif isinstance(v, str):
-            if v.lower() in ("yes", "true", "t", "y"):
-                return True
-            elif v.lower() in ("no", "false", "f", "n"):
-                return False
-            else:
-                raise ValueError(f'Boolean value expected (got {v})')
-        else:
-            raise ValueError(f'Boolean value expected (got {v})')
-    except:
-        # just pitch it back and let the caller worry about it
-        return v
 
 
 ###################################################################################################
@@ -230,6 +195,47 @@ def isipaddress(value):
 
 
 ###################################################################################################
+# attempt to decode a string as JSON, returning the object if it decodes and None otherwise
+def LoadStrIfJson(jsonStr):
+    try:
+        return json.loads(jsonStr)
+    except ValueError as e:
+        return None
+
+
+###################################################################################################
+# attempt to decode a file (given by handle) as JSON, returning the object if it decodes and
+# None otherwise
+def LoadFileIfJson(fileHandle):
+    try:
+        return json.load(fileHandle)
+    except ValueError as e:
+        return None
+
+
+###################################################################################################
+# a context manager for entering a directory and leaving it upon leaving the context
+@contextlib.contextmanager
+def pushd(directory):
+    prevDir = os.getcwd()
+    os.chdir(directory)
+    try:
+        yield
+    finally:
+        os.chdir(prevDir)
+
+
+###################################################################################################
+# strip a prefix from the beginning of a string if needed
+def remove_prefix(text, prefix):
+    if (len(prefix) > 0) and text.startswith(prefix):
+        return text[len(prefix) :]
+    else:
+        return text
+
+
+###################################################################################################
+# return true if os.path.samefile, also False on exception
 def same_file_or_dir(path1, path2):
     try:
         return os.path.samefile(path1, path2)
@@ -250,56 +256,65 @@ def sha256sum(filename):
 
 
 ###################################################################################################
-# recursive dictionary key search
-def dictsearch(d, target):
-    val = filter(
-        None, [[b] if a == target else dictsearch(b, target) if isinstance(b, dict) else None for a, b in d.items()]
-    )
-    return [i for b in val for i in b]
+# nice human-readable file sizes
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
 
 
 ###################################################################################################
-# atomic integer class and context manager
-class AtomicInt:
-    def __init__(self, value=0):
-        self.val = RawValue('i', value)
-        self.lock = Lock()
-
-    def increment(self):
-        with self.lock:
-            self.val.value += 1
-            return self.val.value
-
-    def decrement(self):
-        with self.lock:
-            self.val.value -= 1
-            return self.val.value
-
-    def value(self):
-        with self.lock:
-            return self.val.value
-
-    def __enter__(self):
-        return self.increment()
-
-    def __exit__(self, type, value, traceback):
-        return self.decrement()
+# convenient boolean argument parsing
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise ValueError('Boolean value expected.')
 
 
 ###################################################################################################
-# atomic integer class and context manager
-class ContextLockedOrderedDict(OrderedDict):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.lock = Lock()
+# a context manager returning a temporary filename which is deleted upon leaving the context
+@contextlib.contextmanager
+def temporary_filename(suffix=None):
+    try:
+        f = tempfile.NamedTemporaryFile(suffix=suffix, delete=False)
+        tmp_name = f.name
+        f.close()
+        yield tmp_name
+    finally:
+        os.unlink(tmp_name)
 
-    def __enter__(self):
-        self.lock.acquire()
-        return self
 
-    def __exit__(self, type, value, traceback):
-        self.lock.release()
-        return self
+###################################################################################################
+# open a file and close it, updating its access time
+def touch(filename):
+    open(filename, 'a').close()
+    os.utime(filename, None)
+
+
+###################################################################################################
+def val2bool(v):
+    try:
+        if v is None:
+            return False
+        elif isinstance(v, bool):
+            return v
+        elif isinstance(v, str):
+            if v.lower() in ("yes", "true", "t", "y"):
+                return True
+            elif v.lower() in ("no", "false", "f", "n"):
+                return False
+            else:
+                raise ValueError(f'Boolean value expected (got {v})')
+        else:
+            raise ValueError(f'Boolean value expected (got {v})')
+    except:
+        # just pitch it back and let the caller worry about it
+        return v
 
 
 ###################################################################################################
