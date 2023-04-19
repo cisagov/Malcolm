7c49ae22371a17b3f95779b94eaef02c022eed21
SG <mero.mero.guero@gmail.com>
2023-04-19 10:26:49 -0600

    for idaholab/Malcolm#186, use runtime-logs share for Arkime logs

--- arkime/scripts/viewer_service.sh
+++ arkime/scripts/viewer_service.sh
@@ -6,8 +6,9 @@ while true; do
   if [[ -f /var/run/arkime/initialized && "$VIEWER" == "on" ]]; then
     echo "Launch viewer..."
     rm -f $ARKIME_DIR/logs/viewer*
-    cd $ARKIME_DIR/viewer
+    pushd $ARKIME_DIR/viewer >/dev/null 2>&1
     $ARKIME_DIR/bin/node viewer.js --insecure -c $ARKIME_DIR/etc/config.ini | tee -a $ARKIME_DIR/logs/viewer.log 2>&1
+    popd >/dev/null 2>&1
   fi
   sleep 5
 done
