da0fc2d54522da1a42aab32d05195ba6495890ee
SG <mero.mero.guero@gmail.com>
2023-05-22 16:44:16 -0600

    bump fluent bit for windows setup script to v2.1.3 (https://fluentbit.io/announcements/v2.1.3/)

--- scripts/third-party-logs/fluent-bit-setup.ps1
+++ scripts/third-party-logs/fluent-bit-setup.ps1
@@ -9,7 +9,7 @@
 ###############################################################################
 
 $fluent_bit_version = '2.1'
-$fluent_bit_full_version = '2.1.2'
+$fluent_bit_full_version = '2.1.3'
 
 ###############################################################################
 # select an item from a menu provided in an array
