972ebf730396a3e0c49a160738ba873adfb99649
Seth Grover <mero.mero.guero@gmail.com>
2023-08-09 10:57:03 -0600

    replace jquery file upload with filepond (idaholab/Malcolm#235, work in progress)

--- file-upload/docker-entrypoint.sh
+++ file-upload/docker-entrypoint.sh
@@ -2,17 +2,14 @@
 
 # Copyright (c) 2023 Battelle Energy Alliance, LLC.  All rights reserved.
 
-
-if [[-z $MALCOLM_USERNAME || -z $MALCOLM_PASSWORD ]]
-then
+if [[ -z $MALCOLM_USERNAME || -z $MALCOLM_PASSWORD ]]; then
   echo "Please set the SSH username and (openssl-encrypted then base64-encoded) password by adding the following arguments to docker run/create:"
   echo "  -e MALCOLM_USERNAME='...'"
   echo "  -e MALCOLM_PASSWORD='...'"
   exit 1
 fi
 
-if ! getent passwd "$MALCOLM_USERNAME" >/dev/null
-then
+if ! getent passwd "$MALCOLM_USERNAME" >/dev/null; then
   # Make sure every container gets its own SSH host keys the first time around
   rm -f /etc/ssh/ssh_host_*
   dpkg-reconfigure openssh-server
