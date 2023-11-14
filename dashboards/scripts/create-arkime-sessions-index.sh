fd6d05012bc176e4c0d71d6ffdf46e65094f5757
SG <mero.mero.guero@gmail.com>
2023-11-14 08:47:35 -0700

    idaholab/Malcolm#286, strip out broken Arkime and NetBox links from dashboards for Kibana import

--- dashboards/scripts/create-arkime-sessions-index.sh
+++ dashboards/scripts/create-arkime-sessions-index.sh
@@ -194,9 +194,14 @@ if [[ "$CREATE_OS_ARKIME_SESSION_INDEX" = "true" ]] ; then
           echo "Importing $DATASTORE_TYPE Dashboards saved objects..."
 
           # install default dashboards
-          for i in /opt/dashboards/*.json; do
+          DASHBOARDS_IMPORT_DIR="$(mktemp -d -t dashboards-XXXXXX)"
+          cp /opt/dashboards/*.json "${DASHBOARDS_IMPORT_DIR}"/
+          for i in "${DASHBOARDS_IMPORT_DIR}"/*.json; do
+            # strip out Arkime and NetBox links from dashboards' navigation pane when doing Kibana import (idaholab/Malcolm#286)
+            [[ "$DATASTORE_TYPE" == "elasticsearch" ]] && sed -i 's/  \\\\n\[↪ NetBox\](\/netbox\/)  \\\\n\[↪ Arkime\](\/sessions)//' "$i"
             curl "${CURL_CONFIG_PARAMS[@]}" -L --silent --output /dev/null --show-error -XPOST "$DASHB_URL/api/$DASHBOARDS_URI_PATH/dashboards/import?force=true" -H "$XSRF_HEADER:true" -H 'Content-type:application/json' -d "@$i"
           done
+          rm -rf "${DASHBOARDS_IMPORT_DIR}"
 
           # beats will no longer import its dashbaords into OpenSearch
           # (see opensearch-project/OpenSearch-Dashboards#656 and
