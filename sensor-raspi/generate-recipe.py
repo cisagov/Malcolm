4f737295e56f59dc2f58dbb57c0a9a1d4578b913
Seth Grover <mero.mero.guero@gmail.com>
2024-01-19 14:14:04 -0700

    work in progress on rpi

--- sensor-raspi/generate-recipe.py
+++ sensor-raspi/generate-recipe.py
@@ -92,9 +92,10 @@ hostname = 'Hedgehog-rpi-%s' % version
 # Nothing yet!
 extra_root_shell_cmds = [
     'cp sensor_install.sh "${ROOT?}/root/"',
-    '/bin/bash -c \'mkdir -p "${ROOT?}/opt/"{buildshared,deps,hooks,patches,sensor,arkime/etc,zeek/bin}\'',
+    '/bin/bash -c \'mkdir -p "${ROOT?}/opt/"{buildshared,deps,hooks,patches,sensor/sensor_ctl/suricata/rules-default,arkime/etc,zeek/bin}\'',
     'cp "%s/arkime/patch/"* "${ROOT?}/opt/patches/" || true' % MALCOLM_DIR,
     'cp "%s/arkime/etc/"* "${ROOT?}/opt/arkime/etc" || true' % SENSOR_DIR,
+    'cp "%s/suricata/rules-default/"* "${ROOT?}/opt/sensor/sensor_ctl/suricata/rules-default/" || true' % MALCOLM_DIR,
     'cp -r shared/* "${ROOT?}/opt/buildshared"',
     'cp -r "%s/interface/"* "${ROOT?}/opt/sensor"' % SENSOR_DIR,
     'cp -r "%s/shared/bin/"* "${ROOT?}/usr/local/bin"' % MALCOLM_DIR,
