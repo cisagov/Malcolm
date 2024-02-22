def966194380e3bc9dada0626433a6edf80bb2b5
SG <mero.mero.guero@gmail.com>
2024-02-22 13:13:19 -0700

    Revert "bump logstash and beats to v8.12.2"
    This reverts commit 862d3d58cbb61dae803a0f514dadbc838f6d674c.

--- hedgehog-raspi/sensor_install.sh
+++ hedgehog-raspi/sensor_install.sh
@@ -32,7 +32,7 @@ SHARED_DIR='/opt/buildshared'
 WORK_DIR="$(mktemp -d -t hedgehog-XXXXXX)"
 SENSOR_DIR='/opt/sensor'
 
-BEATS_VER="8.12.2"
+BEATS_VER="8.12.1"
 BEATS_OSS="-oss"
 
 # Option to build from sources if desired
