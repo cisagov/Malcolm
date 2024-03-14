51ffa938d09d5d4b0c691015f47ccabceaf67c9b
Seth Grover <seth.d.grover@gmail.com>
2024-03-14 10:27:42 -0600

    added release_carver.ps1, see idaholab/Malcolm#440

--- scripts/release_cleaver.ps1
+++ scripts/release_cleaver.ps1
@@ -2,34 +2,19 @@
 
 # Copyright (c) 2024 Battelle Energy Alliance, LLC.  All rights reserved.
 
+# release_cleaver.sh
 # Split and join large files into 2 gigabyte chunks. sha256 sum is
-# also calculated and saved on split and checked on join.
+#   also calculated and saved on split and checked on join.
 
-if (-not $PSVersionTable.PSVersion) {
-    Write-Host "Wrong interpreter, please run \"$($MyInvocation.MyCommand.Path)\" with PowerShell"
-    exit 1
-}
-
-if (-not (Get-Command -Name "Split-Path" -ErrorAction SilentlyContinue) -or
-    -not (Get-Command -Name "Get-FileHash" -ErrorAction SilentlyContinue) -or
-    -not (Get-Command -Name "Join-Path" -ErrorAction SilentlyContinue) -or
-    -not (Get-Command -Name "cat" -ErrorAction SilentlyContinue)) {
-    Write-Error "$($MyInvocation.MyCommand.Path) requires Split-Path, Get-FileHash, Join-Path and cat"
-    exit 1
-}
 
 $ErrorActionPreference = "Stop"
 
-function Get-BaseName {
-    param([string]$path)
-    return (Split-Path -Path $path -Leaf).Split(".")[0]
-}
 
-function Get-Extension {
-    param([string]$path)
-    return (Split-Path -Path $path -Leaf).Split(".")[-1]
-}
-
+# Split a binary file into a series of smaller files
+#  - FilePath - path to file to be split
+#  - OutDir - directory containing resultant fragment files
+#  - ChunkSize - maximum size of each file part
+#  - BufferSize - intermediate in-memory buffer size
 function Split-BinaryFile {
     param (
         [string]$FilePath,
@@ -39,7 +24,6 @@ function Split-BinaryFile {
     )
 
     $fileStream = [System.IO.File]::OpenRead($FilePath)
-
     try {
         $chunkIndex = 1
         $bytesReadTotal = 0
@@ -70,6 +54,9 @@ function Split-BinaryFile {
     }
 }
 
+# Split a binary file into a series of smaller files
+#  - FilePaths - array of files to join (in the order to be reassembled)
+#  - OutputFile - Filename of resulting joined file
 function Concatenate-BinaryFiles {
     param (
         [string[]]$FilePaths,
@@ -93,21 +80,112 @@ function Concatenate-BinaryFiles {
     }
 }
 
+# first expand wildcard arguments ($args -> $allFileArgs)
+$allFileArgs = @()
+foreach ($filename in $args) {
+    $expandedFiles = Get-ChildItem -Path $filename
+    foreach ($expandedFile in $expandedFiles) {
+        If (-not ($allFileArgs -contains $expandedFile)) {
+            $allFileArgs += $expandedFile.FullName
+        }
+    }
+}
 
-if ($args.Count -eq 0) {
+if ($allFileArgs.Count -eq 0) {
     Write-Host "Usage:"
     Write-Host "  $(Split-Path -Path $MyInvocation.MyCommand.Path -Leaf) <file_to_split>"
     Write-Host "OR"
     Write-Host "  $(Split-Path -Path $MyInvocation.MyCommand.Path -Leaf) <file_to_join.00> <file_to_join.01> ... <file_to_join.sha>"
     exit 1
 
-} elseif ($args.Count -gt 1) {
-    # TODO:
-    # Concatenate-BinaryFiles -FilePaths $args[0..($args.Length - 1)] -OutputFile $args[-1]
+} elseif ($allFileArgs.Count -gt 1) {
+    Write-Host "Joining..."
+
+    # separate the sha file from the files to join
+    $shaFiles = @()
+    $splitFiles = @()
+    foreach ($filename in $allFileArgs) {
+        if (Test-Path $filename -PathType Leaf) {
+            if ($filename -like "*.sha") {
+                $shaFiles += $filename
+            } else {
+                $splitFiles += $filename
+            }
+        } else {
+            Write-Host """$($filename)"" does not exist"
+            exit 1
+        }
+    }
+
+    # make sure the base names of the files to join match
+    $prevBase = ""
+    foreach ($filename in $splitFiles) {
+        $curBase = [System.IO.Path]::GetFileNameWithoutExtension($filename);
+        if ($prevBase -and ($prevBase -ne $curBase)) {
+            Write-Host "File basenames ""$($prevBase)"" and ""$($curBase)"" do not match, giving up"
+            exit 1
+        } else {
+            $prevBase = $curBase
+        }
+    }
+    $outFile = $prevBase
+
+    # don't overwrite an existing file
+    if (Test-Path $outFile -PathType Leaf) {
+        Write-Host """$($outFile)"" already exists"
+        exit 1
+    }
+
+    # join the files
+    Concatenate-BinaryFiles $splitFiles $outFile
+
+    # check the results and sha sum
+    if (Test-Path $outFile -PathType Leaf) {
+        $outFileItem = Get-Item $outFile
+        if ($outFileItem.Length -gt 0) {
+            if ($shaFiles.Count -ne 1) {
+                Write-Host "Files joined to ""$($outFile)"", but could not verify file integrity"
+                exit 1
+
+            } else {
+                # calculate the sha256 sum
+                $outFileHash = Get-FileHash -Path $outFile -Algorithm SHA256
+                $outFileHashSha256 = $outFileHash.Hash.ToLower()
+
+                # Read the contents of the sha file for comparison
+                $shaFileContent = Get-Content $shaFiles[0]
+                $shaFileContents = @()
+                foreach ($line in $shaFileContent) {
+                    $parts = $line -split '\s+'
+                    if ($parts.Length -eq 2) {
+                        $shaFileContents += @($parts[0].ToLower(), $parts[1])
+                        break
+                    }
+                }
+
+                # compare the joined file and hash from the sha file
+                if ($shaFileContents[0] -eq $outFileHashSha256.ToLower()) {
+                    Write-Host """$($outFile)"" OK"
+
+                } else {
+                    Write-Host """$($outFile)"" SHA256 hash mismatch ($($shaFileContents[0]) vs $($outFileHashSha256))"
+                    exit 1
+                }
+            }
+
+        } else {
+            Write-Host "Attempted to join files to ""$($outFile)"", but an empty file resulted"
+            exit 1
+        }
+
+    } else {
+        Write-Host "Attempted to join files to ""$($outFile)"", but could not create the file"
+        exit 1
+    }
 
 } else {
     Write-Host "Splitting..."
-    $fileToSplit = $args[0]
+    $fileToSplit = $allFileArgs[0]
 
     # generate sha256 sum file
     $shaFile = Join-Path -Path (Get-Location) -ChildPath ((Split-Path -Path $fileToSplit -Leaf) + ".sha")
@@ -115,5 +193,6 @@ if ($args.Count -eq 0) {
     Add-Content -Path $shaFile -NoNewline -Value '  '
     Add-Content -Path $shaFile -Value (Split-Path -Path $fileToSplit -Leaf)
 
+    # split the file into its parts
     Split-BinaryFile $fileToSplit (Get-Location)
 }
