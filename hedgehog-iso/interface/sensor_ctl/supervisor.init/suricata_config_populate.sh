699e75c6256d73d22e1c2f00149cf6fd3fb01a3f
Seth Grover <seth.d.grover@gmail.com>
2024-03-18 12:23:34 -0600

    work for idaholab/Malcolm#445, supporting suricata eve.json rotation

--- hedgehog-iso/interface/sensor_ctl/supervisor.init/suricata_config_populate.sh
+++ hedgehog-iso/interface/sensor_ctl/supervisor.init/suricata_config_populate.sh
@@ -2,11 +2,6 @@
 
 if [[ -n $SUPERVISOR_PATH ]] && [[ -r /usr/local/bin/suricata_config_populate.py ]]; then
 
-    # if there's a previous eve.json file, rename it prior to starting up
-    [[ -n $ZEEK_LOG_PATH ]] && [[ -f "$ZEEK_LOG_PATH"/suricata/eve.json ]] && \
-        mv -f "$ZEEK_LOG_PATH/suricata/eve.json" \
-              "$ZEEK_LOG_PATH/suricata/eve.json.$(date -d @$(stat -c%Y "$ZEEK_LOG_PATH/suricata/eve.json") +'%Y%m%d%H%M%S')"
-
     # if there's no configuration files to modify, start with the defaults
     [[ ! -f "$SUPERVISOR_PATH"/suricata/suricata.yaml ]] && cp /etc/suricata/suricata.yaml "$SUPERVISOR_PATH"/suricata/suricata.yaml
     [[ ! -f "$SUPERVISOR_PATH"/suricata/update.yaml ]] && cp "$(dpkg -L suricata-update | grep 'update\.yaml' | head -n 1)" "$SUPERVISOR_PATH"/suricata/update.yaml
