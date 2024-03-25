a24781a69ddaf3a8d2150b46e958c50ec4f8bb8c
Seth Grover <seth.d.grover@gmail.com>
2024-03-25 12:26:18 -0600

    move profinet plugin to point upstream at amzn/zeek-plugin-profinet now that the PR has been pulled

--- shared/bin/zeek_install_plugins.sh
+++ shared/bin/zeek_install_plugins.sh
@@ -66,7 +66,7 @@ ZKG_GITHUB_URLS=(
   "https://github.com/0xl3x1/zeek-EternalSafety"
   "https://github.com/0xxon/cve-2020-0601"
   "https://github.com/0xxon/cve-2020-13777"
-  "https://github.com/mmguero-dev/zeek-plugin-profinet|master"
+  "https://github.com/amzn/zeek-plugin-profinet|master"
   "https://github.com/amzn/zeek-plugin-tds|master"
   "https://github.com/cisagov/icsnpp-bacnet"
   "https://github.com/cisagov/icsnpp-bsap"
