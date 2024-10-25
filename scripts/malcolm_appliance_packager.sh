cb2d710b3ae3127e01a71f854076055f7fb0471c
Seth Grover <seth.d.grover@gmail.com>
2024-10-25 11:18:04 -0600

    packager script tweak

--- scripts/malcolm_appliance_packager.sh
+++ scripts/malcolm_appliance_packager.sh
@@ -29,6 +29,7 @@ if ! (type "$REALPATH" && type "$DIRNAME") > /dev/null; then
 fi
 SCRIPT_PATH="$($DIRNAME $($REALPATH -e "${BASH_SOURCE[0]}"))"
 pushd "$SCRIPT_PATH/.." >/dev/null 2>&1
+MALCOLM_CONTAINER_RUNTIME="${MALCOLM_CONTAINER_RUNTIME:-docker}"
 
 CURRENT_REV_SHA="$(git rev-parse --short --verify HEAD)"
 if [ -z "$CURRENT_REV_SHA" ]; then
@@ -154,7 +155,11 @@ if mkdir "$DESTDIR"; then
     echo "This might take a few minutes..."
     DESTNAMEIMAGES="$RUN_PATH/$(basename $DESTDIR)_images.tar.xz"
     IMAGES=( $(grep image: $DESTDIR/docker-compose.yml | awk '{print $2}' | sort -u) )
-    docker save "${IMAGES[@]}" | xz -1 > "$DESTNAMEIMAGES"
+    if [[ "$MALCOLM_CONTAINER_RUNTIME" == "podman" ]]; then
+      $MALCOLM_CONTAINER_RUNTIME save --multi-image-archive --format docker-archive "${IMAGES[@]}" | xz -1 > "$DESTNAMEIMAGES"
+    else
+      $MALCOLM_CONTAINER_RUNTIME save "${IMAGES[@]}" | xz -1 > "$DESTNAMEIMAGES"
+    fi
     echo "Packaged Malcolm container images to \"$DESTNAMEIMAGES\""
     echo ""
   fi
