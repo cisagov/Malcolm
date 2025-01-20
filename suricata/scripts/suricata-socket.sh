c100e7bb12c43eb815b0a5599564a49e26ddb186
Seth Grover <seth.d.grover@gmail.com>
2025-01-20 15:34:10 -0700

    more flexible verbosity for suricata

--- suricata/scripts/suricata-socket.sh
+++ suricata/scripts/suricata-socket.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-set -e
-set -u
-set -o pipefail
-
-ENCODING="utf-8"
-
 [[ -x /usr/bin/suricata-offline ]] && SURICATA_BIN=/usr/bin/suricata-offline || SURICATA_BIN=/usr/bin/suricata
 SURICATA_CONFIG_FILE=${SURICATA_CONFIG_FILE:-/etc/suricata/suricata.yaml}
 SURICATA_LOG_DIR=${SURICATA_LOG_DIR:-/var/log/suricata}
+[[ -z "$PCAP_PIPELINE_VERBOSITY" ]] && SURICATA_VERBOSE_FLAG= || SURICATA_VERBOSE_FLAG=-v
 
-"${SURICATA_BIN}" -v \
+"${SURICATA_BIN}" "${SURICATA_VERBOSE_FLAG}" \
     --unix-socket \
     --set unix-command.enabled=true \
     -c "${SURICATA_CONFIG_FILE}" \
