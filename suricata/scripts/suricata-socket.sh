68962da186e06e654b294cae70f1cd0036352f09
Seth Grover <seth.d.grover@gmail.com>
2025-01-21 13:53:48 -0700

    for cisagov/Malcolm#457, signal suricata rules to reload after update

--- suricata/scripts/suricata-socket.sh
+++ suricata/scripts/suricata-socket.sh
@@ -5,8 +5,12 @@ SURICATA_CONFIG_FILE=${SURICATA_CONFIG_FILE:-/etc/suricata/suricata.yaml}
 SURICATA_LOG_DIR=${SURICATA_LOG_DIR:-/var/log/suricata}
 SURICATA_VERBOSE_FLAG=${PCAP_PIPELINE_VERBOSITY:-}
 
+SURICATA_RUN_DIR="${SURICATA_RUN_DIR:-}"
+[[ -z "$SURICATA_RUN_DIR" ]] && SURICATA_RUN_DIR="${SUPERVISOR_PATH:-/var/run}/suricata"
+SURICATA_SOCKET="$SURICATA_RUN_DIR"/suricata-command.socket
+
 "${SURICATA_BIN}" "${SURICATA_VERBOSE_FLAG}" \
-    --unix-socket \
+    --unix-socket="$SURICATA_SOCKET" \
     --set unix-command.enabled=true \
     -c "${SURICATA_CONFIG_FILE}" \
     -l "${SURICATA_LOG_DIR}"
