6c4806e496cf833b1443551b7e74a7191f2f81e2
Seth Grover <seth.d.grover@gmail.com>
2025-02-25 07:34:02 -0700

    don't fail healthcheck if pcap_capture is disabled

--- container-health-scripts/pcap-capture.sh
+++ container-health-scripts/pcap-capture.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
+[[ "${PCAP_CAPTURE_DISABLED:-false}" == 'true' ]] && exit 0
+
 (( $(supervisorctl status 2>/dev/null | grep -cPv '(STARTING|RUNNING|Not started$)') == 0 )) && exit 0 || exit 1
+
