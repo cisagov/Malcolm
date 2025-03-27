8b3ad37ba5e27cf2180cf21025bc6826af3a3fe6
Seth Grover <seth.d.grover@gmail.com>
2025-03-27 07:43:06 -0600

    restore X-Remote-Auth header for netbox

--- nginx/lua/nginx_auth_helpers.lua
+++ nginx/lua/nginx_auth_helpers.lua
@@ -5,7 +5,7 @@ local cjson = require("cjson.safe")
 function _M.set_headers(username, token, groups, roles)
     if username ~= nil and username ~= '' then
         ngx.req.set_header("http_auth_http_user", username)
-        ngx.req.set_header("X-Remote-User", username)
+        ngx.req.set_header("X-Remote-Auth", username)
         ngx.req.set_header("X-Remote-User", username)
         ngx.req.set_header("X-Forwarded-User", username)
     end
