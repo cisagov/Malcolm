4b3a422f429f897c42ef0117c2598bb8096e80c0
Seth Grover <seth.d.grover@gmail.com>
2025-04-03 08:12:54 -0600

    avoid superuser api token ending up in process list

--- container-health-scripts/netbox.sh
+++ container-health-scripts/netbox.sh
@@ -3,10 +3,11 @@
 set -euo pipefail
 
 if [[ -n "$SUPERUSER_API_TOKEN" ]]; then
-    curl --silent --output /dev/null --fail \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Token $SUPERUSER_API_TOKEN" \
-        "http://localhost:8080/netbox/api/"
+    echo -n "Authorization: Token $SUPERUSER_API_TOKEN" | \
+        curl --silent --output /dev/null --fail \
+            -H "Content-Type: application/json" \
+            -H @- \
+            "http://localhost:8080/netbox/api/"
 else
     curl --silent --output /dev/null --fail \
         -H "Content-Type: application/json" \
