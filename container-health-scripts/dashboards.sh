33440d88eb18712b866ae75b331c77761d2bb2f4
Seth Grover <seth.d.grover@gmail.com>
2025-05-19 16:11:25 -0600

    cisagov/Malcolm#460, work in progress for roles

--- container-health-scripts/dashboards.sh
+++ container-health-scripts/dashboards.sh
@@ -2,4 +2,14 @@
 
 set -euo pipefail
 
-curl --silent --output /dev/null --fail "http://localhost:5601/dashboards/api/status"
+OPENSEARCH_CREDS_CONFIG_FILE=${OPENSEARCH_CREDS_CONFIG_FILE:-"/var/local/curlrc/.opensearch.primary.curlrc"}
+if [[ -r "$OPENSEARCH_CREDS_CONFIG_FILE" ]]; then
+  CURL_CONFIG_PARAMS=(
+    --config
+    "$OPENSEARCH_CREDS_CONFIG_FILE"
+    )
+else
+  CURL_CONFIG_PARAMS=()
+fi
+
+curl "${CURL_CONFIG_PARAMS[@]}" --insecure --silent --output /dev/null --fail "http://localhost:5601/dashboards/api/status"
