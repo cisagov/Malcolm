39dd614a9c7ea052584864e985f6ef1e520e6c3e
Seth Grover <seth.d.grover@gmail.com>
2025-05-20 13:41:11 -0600

    cisagov/Malcolm#460, work in progress for roles

--- opensearch-config/scripts/setup-internal-users.sh
+++ opensearch-config/scripts/setup-internal-users.sh
@@ -3,6 +3,8 @@
 CREDS_CONFIG_FILE=${OPENSEARCH_CREDS_CONFIG_FILE:-"/var/local/curlrc/.opensearch.primary.curlrc"}
 OPENSEARCH_SECURITY_CERTS_DIR=/usr/share/opensearch/config/certs
 INTERNAL_USERS_YML=/usr/share/opensearch/config/opensearch-security/internal_users.yml
+ROLE_MAPPING_YML_ORIG=/usr/share/opensearch/config/opensearch-security/roles_mapping.yml.orig
+ROLE_MAPPING_YML=/usr/share/opensearch/config/opensearch-security/roles_mapping.yml
 
 # get the new username/password from the curl file (I already wrote python code to do this, so sue me)
 pushd "$(dirname $(realpath -e "${BASH_SOURCE[0]}"))" >/dev/null 2>&1
@@ -11,6 +13,7 @@ INTERNAL_USERNAME="$(echo "$INTERNAL_CREDS" | cut -d'|' -f1)"
 INTERNAL_PASSWORD="$(INTERNAL_PASSWORD="$(echo "$INTERNAL_CREDS" | cut -d'|' -f2)" /usr/share/opensearch/plugins/opensearch-security/tools/hash.sh -env INTERNAL_PASSWORD)"
 popd >/dev/null 2>&1
 
+# generate internal_users.yml
 if [[ -n "${INTERNAL_USERNAME}" ]] && [[ -n "${INTERNAL_PASSWORD}" ]]; then
   cat <<EOF > "$INTERNAL_USERS_YML"
 ---
@@ -34,6 +37,14 @@ _meta:
 EOF
 fi
 
+# do replacers from environment bariables for roles_mapping.yml
+if [[ -r "${ROLE_MAPPING_YML_ORIG}" ]]; then
+  envsubst < "${ROLE_MAPPING_YML_ORIG}" \
+    | yq 'del(.[] | select(.backend_roles and (.backend_roles[] == "")))' \
+    > "${ROLE_MAPPING_YML}"
+fi
+
+# generate self-signed keys wanted by opensearch security plugin (only used internally)
 [[ -x /usr/local/bin/self_signed_key_gen.sh ]] && \
   /usr/local/bin/self_signed_key_gen.sh -n -p \
     -o "${OPENSEARCH_SECURITY_CERTS_DIR}" \
@@ -43,5 +54,6 @@ fi
     mv "${OPENSEARCH_SECURITY_CERTS_DIR}"/{client,admin}.crt && \
     mv "${OPENSEARCH_SECURITY_CERTS_DIR}"/{client,admin}.key
 
+# background setup processes to run after opensearch starts
 [[ -x /usr/local/bin/setup-post-start.sh ]] && \
   nohup /usr/local/bin/setup-post-start.sh >/dev/null 2>&1 </dev/null &
