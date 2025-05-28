3080617ea77e74c5a4b838fc05fe82c470b5b2a4
Melanie Pierce <melanie.pierce@inl.gov>
2025-05-28 09:46:21 -0600

    Mount directory in Arkime

--- arkime/scripts/wise_service.sh
+++ arkime/scripts/wise_service.sh
@@ -7,7 +7,7 @@ while true; do
     echo "Launch wise..."
     rm -f $ARKIME_DIR/logs/wise*
     pushd $ARKIME_DIR/wiseService >/dev/null 2>&1
-    $ARKIME_DIR/bin/node wiseService.js --insecure -c $ARKIME_DIR/etc/wise.ini
+    $ARKIME_DIR/bin/node wiseService.js --insecure -c $ARKIME_DIR/wiseini/wise.ini
     popd >/dev/null 2>&1
   fi
   sleep 5
