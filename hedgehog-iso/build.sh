9b7ef84495d2d83b4820c9806a0c691fadbb3241
Seth Grover <seth.d.grover@gmail.com>
2025-07-03 21:39:15 +0000

    rename file

--- hedgehog-iso/build.sh
+++ hedgehog-iso/build.sh
@@ -118,7 +118,7 @@ if [ -d "$WORKDIR" ]; then
   rsync -a "$SCRIPT_PATH/shared/bin/" ./config/includes.chroot/usr/local/bin/
   mkdir -p ./config/includes.chroot/opt/zeek/bin/
   mv ./config/includes.chroot/usr/local/bin/zeekdeploy.sh ./config/includes.chroot/opt/zeek/bin/
-  mv ./config/includes.chroot/usr/local/bin/iana_lookup_zeek_generator.py ./config/includes.chroot/opt/zeek/bin/
+  mv ./config/includes.chroot/usr/local/bin/zeek_iana_lookup_generator.py ./config/includes.chroot/opt/zeek/bin/
   ln -s -r ./config/includes.chroot/usr/local/bin/malcolm_utils.py ./config/includes.chroot/opt/zeek/bin/
   chown -R root:root ./config/includes.chroot/usr/local/bin/ ./config/includes.chroot/opt/zeek/bin/
   rsync -a "$SCRIPT_PATH/suricata/" ./config/includes.chroot/opt/sensor/sensor_ctl/suricata/
--- hedgehog-raspi/sensor_install.sh
+++ hedgehog-raspi/sensor_install.sh
@@ -358,7 +358,7 @@ install_files() {
     # Shared Scripts setup
     ln -s /usr/local/bin/malcolm_utils.py "/opt/zeek/bin/"
     mv /usr/local/bin/zeekdeploy.sh "/opt/zeek/bin/"
-    mv /usr/local/bin/iana_lookup_zeek_generator.py "/opt/zeek/bin/"
+    mv /usr/local/bin/zeek_iana_lookup_generator.py "/opt/zeek/bin/"
     rm -rf /usr/local/bin/aide_integrity_check.sh
 
     # Setup OS information
