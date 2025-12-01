e04e198ce8329fd704d077e69d8df71cc1274aa1
Seth Grover <seth.d.grover@gmail.com>
2025-12-01 18:54:47 +0000

    bump revision

--- scripts/control.py
+++ scripts/control.py
@@ -3086,7 +3086,7 @@ def main():
         metavar='<string>',
         type=str,
         default=os.getenv('MALCOLM_IMAGE_TAG', None),
-        help='Tag for container images (e.g., "25.12.0"; only for "start" operation with Kubernetes)',
+        help='Tag for container images (e.g., "26.01.0"; only for "start" operation with Kubernetes)',
     )
     kubernetesGroup.add_argument(
         '--delete-namespace',
